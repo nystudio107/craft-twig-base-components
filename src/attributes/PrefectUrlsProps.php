83f44ce57cae810a1c75d507e0f7eff4bcf48924
Andrew Welch <andrew@nystudio107.com>
2023-12-18 19:30:13 -0500

    feat: Initial checkin

---
+++ src/attributes/PrefectUrlsProps.php
@@ -0,0 +1,17 @@
+<?php
+namespace nystudio107\craftTwigBaseComponents\attributes;
+
+use Attribute;
+
+#[Attribute]
+class PrefectUrlsProps
+{
+    /* @var string[] $prefetchUrls The URLs to prefetch */
+    public array $prefetchUrls = [];
+
+    /* @var bool $outputLinks Whether <link> tags should be output */
+    public bool $outputLinks = true;
+
+    /* @var bool $outputHeaders Whether Link: headers should be output */
+    public bool $outputHeaders = true;
+}
